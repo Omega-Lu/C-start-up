705b07f160594d33f1f7cfa4788036970e535dab
Luthando-D <62010426+Luthando-D@users.noreply.github.com>
2023-10-23 09:27:24 +0200

    Add project files.

---
+++ HelloWorld.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace WindowsFormsApp6
+{
+    public partial class HelloWorld : Form
+    {
+        public HelloWorld()
+        {
+            InitializeComponent();
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+            
+        }
+
+        private void HelloWorld_Load(object sender, EventArgs e)
+        {
+            string myLabel = "Hello World";
+            label1.Text = myLabel;
+        }
+    }
+}
